2d4f8cbc5d38a4a202ca405746138fbdd681efc5
Kito <vcgolem@gmail.com>
2026-05-11 22:53:11 -0300

    nixos rebuild: switch

--- home/packages/desktop.nix
+++ home/packages/desktop.nix
@@ -16,7 +16,6 @@
     prismlauncher
     inputs.ags.packages.${pkgs.system}.agsFull
     inkscape
-    inputs.zen-browser.packages.x86_64-linux.default
     inputs.hytale-launcher.packages.x86_64-linux.default
   ];
 }
