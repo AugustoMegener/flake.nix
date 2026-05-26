a13b0fdb7759a07a2519e84d4ebbb3e48b15c74d
Kito <vcgolem@gmail.com>
2026-05-26 16:31:38 -0300

    nixos rebuild: switch

--- systems/nixvim/config/plugins/smear-cursor/default.nix
+++ systems/nixvim/config/plugins/smear-cursor/default.nix
@@ -3,25 +3,28 @@
 
   plugins.smear-cursor = {
     enable = true;
-    cursor_color = lib.nixvim.mkRaw ''
-      function()
-      return {
-        n = "#f29554",
-        i = "#6bc99d",
-        v = "#9595d9",
-        V = "#9595d9",
-        ["\22"] = "#9595d9",
-        R = "#f25146",
-        c = "#e3a824",
-      }[vim.fn.mode()] or "#f29554"
-    end
-      '';
-    opts = {
-      smear_between_buffers = true;
-      smear_between_neighbor_lines = true;
-      scroll_buffer_space = true;
-      legacy_computing_symbols_support = false;
-      smear_insert_mode = true;
+    settings = {
+      cursor_color = lib.nixvim.mkRaw ''
+        function()
+          local colors = {
+            n = "#f29554",
+            i = "#6bc99d",
+            v = "#9595d9",
+            V = "#9595d9",
+            ["\22"] = "#9595d9",
+            R = "#f25146",
+            c = "#e3a824",
+          }
+          return colors[vim.fn.mode()] or "#f29554"
+        end
+        '';
+      opts = {
+        smear_between_buffers = true;
+        smear_between_neighbor_lines = true;
+        scroll_buffer_space = true;
+        legacy_computing_symbols_support = false;
+        smear_insert_mode = true;
+      };
     };
   };
 
