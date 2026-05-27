a5763e6086b221733097f7d8ec31ca897dd5c32e
Kito <vcgolem@gmail.com>
2026-05-27 12:13:42 -0300

    nixos rebuild: switch

--- home/programs/zsh.nix
+++ home/programs/zsh.nix
@@ -8,7 +8,7 @@
     sessionVariables = {
       TMUX_TMPDIR = "$XDG_RUNTIME_DIR";
     };
-    initExtra = lib.mkAfter ''
+    initContent = lib.mkAfter ''
       if [[ -z "$TMUX" && $- == *i* && "$TERM" != "dumb" && -z "$ZSH_EXECUTION_STRING" ]]; then
         if tmux list-sessions -F '#{session_name}:#{session_attached}' 2>/dev/null | grep -v '^yazi:' | grep ':0$' | head -1 | grep -q .; then
           exec tmux attach-session -t "$(tmux list-sessions -F '#{session_name}:#{session_attached}' 2>/dev/null | grep -v '^yazi:' | grep ':0$' | head -1 | cut -d: -f1)"
