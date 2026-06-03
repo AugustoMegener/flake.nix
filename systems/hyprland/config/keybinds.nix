19278711b3d44030b7f76b79b0c252cea4d28054
Kito <vcgolem@gmail.com>
2026-06-03 20:13:00 -0300

    nixos rebuild: switch

--- systems/hyprland/config/keybinds.nix
+++ systems/hyprland/config/keybinds.nix
@@ -44,9 +44,9 @@
       "$mainMod SHIFT, S, movetoworkspace, special:magic"
       "$mainMod, mouse_down, workspace, e+1"
       "$mainMod, mouse_up, workspace, e-1"
-      "$mod, Escape, exec, hyprshutdown --post-cmd 'systemctl hibernate'"
-      "$mod SHIFT, Escape, exec, hyprshutdown --post-cmd 'systemctl poweroff'"
-      "$mod CTRL, Escape, exec, hyprshutdown --post-cmd 'systemctl reboot'"
+      "$mainMod, Escape, exec, hyprshutdown --post-cmd 'systemctl hibernate'"
+      "$mainMod SHIFT, Escape, exec, hyprshutdown --post-cmd 'systemctl poweroff'"
+      "$mainMod CTRL, Escape, exec, hyprshutdown --post-cmd 'systemctl reboot'"
     ];
 
     bindm = [
