ec6a79d184901ea760818648ad847e7289166598
AugustoMegener <76854495+AugustoMegener@users.noreply.github.com>
2026-06-04 23:48:42 -0300

    nixos rebuild: switch

--- home/scripts/init-tmux.sh
+++ home/scripts/init-tmux.sh
@@ -1,7 +1,11 @@
-if [[ -z "$tmux" && $- == *i* && "$term" != "dumb" && -z "$zsh_execution_string" ]]; then
-  if tmux list-sessions -f '#{session_name}:#{session_attached}' 2>/dev/null | grep -v '^yazi:' | grep ':0$' | head -1 | grep -q .; then
-    tmux attach-session -t "$(tmux list-sessions -f '#{session_name}:#{session_attached}' 2>/dev/null | grep -v '^yazi:' | grep ':0$' | head -1 | cut -d: -f1)"
+cmd="$*"
+
+if [[ -z "$tmux" && "$term" != "dumb" && -z "$zsh_execution_string" ]]; then
+  target="$(tmux list-sessions -f '#{session_name}:#{session_attached}' 2>/dev/null | grep -v '^yazi:' | grep ':0$' | head -1 | cut -d: -f1)"
+
+  if [[ -n "$target" ]]; then
+    tmux attach-session -t "$target"
   else
-    tmux new-session -s "$(tmux-next-session)"
+    tmux new-session -s "$(tmux-next-session)" "$cmd"
   fi
 fi
