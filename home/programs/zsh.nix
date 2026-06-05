5724b27ab66e12a0dd1ff17b08ee3d94f71ab517
AugustoMegener <76854495+AugustoMegener@users.noreply.github.com>
2026-06-05 09:59:02 -0300

    nixos rebuild: switch

--- home/programs/zsh.nix
+++ home/programs/zsh.nix
@@ -13,6 +13,13 @@
         source "$(fzf-share)/key-bindings.zsh"
         source "$(fzf-share)/completion.zsh"
       fi
-    '';
+      export FZF_DEFAULT_OPTS=$'--border=rounded
+        --list-border=rounded
+        --preview-border=rounded
+        --color=fg:#d5bea1,bg:#302b24,hl:#da9a22,fg+:#ebdbb2,bg+:#3c3836
+        --color=hl+:#fabd2f,info:#4396b7,prompt:#bdae93,pointer:#4396b7
+        --color=marker:#4396b7,spinner:#da9a22,header:#a89984,border:#d5bea1
+        --color=gutter:#3c3836'
+          '';
   };
 }
