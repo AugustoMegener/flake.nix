eaa39f745b7f0031f6e6bde4402ee0a33d32d6a5
AugustoMegener <76854495+AugustoMegener@users.noreply.github.com>
2026-06-05 10:02:54 -0300

    nixos rebuild: switch

--- home/programs/zsh.nix
+++ home/programs/zsh.nix
@@ -7,19 +7,20 @@
     oh-my-zsh.enable = true;
     sessionVariables = {
       TMUX_TMPDIR = "$XDG_RUNTIME_DIR";
-    };
-    initContent = lib.mkAfter ''
-      if [ -n "''${commands[fzf-share]}" ]; then
-        source "$(fzf-share)/key-bindings.zsh"
-        source "$(fzf-share)/completion.zsh"
-      fi
-      export FZF_DEFAULT_OPTS=$'--border=rounded
+      FZF_DEFAULT_OPTS=''$'--border=rounded
         --list-border=rounded
         --preview-border=rounded
         --color=fg:#d5bea1,bg:#302b24,hl:#da9a22,fg+:#ebdbb2,bg+:#3c3836
         --color=hl+:#fabd2f,info:#4396b7,prompt:#bdae93,pointer:#4396b7
         --color=marker:#4396b7,spinner:#da9a22,header:#a89984,border:#d5bea1
         --color=gutter:#3c3836'
-          '';
+      '';
+    };
+    initContent = lib.mkAfter ''
+      if [ -n "''${commands[fzf-share]}" ]; then
+        source "$(fzf-share)/key-bindings.zsh"
+        source "$(fzf-share)/completion.zsh"
+      fi
+      '';
   };
 }
