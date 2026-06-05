33502dfd6375eb800eaf57618c281af1d99b549b
AugustoMegener <76854495+AugustoMegener@users.noreply.github.com>
2026-06-04 22:59:01 -0300

    nixos rebuild: switch

--- home/programs/zsh.nix
+++ home/programs/zsh.nix
@@ -9,13 +9,6 @@
       TMUX_TMPDIR = "$XDG_RUNTIME_DIR";
     };
     initContent = lib.mkAfter ''
-      if [[ -z "$TMUX" && $- == *i* && "$TERM" != "dumb" && -z "$ZSH_EXECUTION_STRING" ]]; then
-        if tmux list-sessions -F '#{session_name}:#{session_attached}' 2>/dev/null | grep -v '^yazi:' | grep ':0$' | head -1 | grep -q .; then
-          exec tmux attach-session -t "$(tmux list-sessions -F '#{session_name}:#{session_attached}' 2>/dev/null | grep -v '^yazi:' | grep ':0$' | head -1 | cut -d: -f1)"
-        else
-          exec tmux new-session -s "$(tmux-next-session)"
-        fi
-      fi
     '';
   };
 }
