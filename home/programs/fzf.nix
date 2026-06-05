afa24f866dbff315a1e9a7ec1239ad35ade9572b
AugustoMegener <76854495+AugustoMegener@users.noreply.github.com>
2026-06-05 10:56:04 -0300

    nixos rebuild: switch

--- home/programs/fzf.nix
+++ home/programs/fzf.nix
@@ -2,5 +2,14 @@
 {
   programs.fzf = {
     enable = true;
+    defaultOptions = [
+      "--border=rounded"
+      "--list-border=rounded"
+      "--preview-border=rounded"
+      "--color=fg:#d5bea1,bg:#302b24,hl:#da9a22,fg+:#ebdbb2,bg+:#3c3836"
+      "--color=hl+:#fabd2f,info:#4396b7,prompt:#bdae93,pointer:#4396b7"
+      "--color=marker:#4396b7,spinner:#da9a22,header:#a89984,border:#d5bea1"
+      "--color=gutter:#3c3836"
+    ];
   };
 }
