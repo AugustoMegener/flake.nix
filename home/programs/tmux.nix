2672315777128e2d89f89d2aa993e6103702dd82
AugustoMegener <76854495+AugustoMegener@users.noreply.github.com>
2026-06-07 13:30:09 -0300

    nixos rebuild: switch

--- home/programs/tmux.nix
+++ home/programs/tmux.nix
@@ -14,7 +14,7 @@
 
     extraConfig = ''
       set -gq allow-passthrough on 
-      set-hook -g client-detached 'run-shell "tmux list-panes -t #{session_name} -F #{pane_pid} | xargs -I{} ps --ppid {} -o comm= | grep -qvE \"^(zsh|yazi)$\" || tmux kill-session -t #{session_name}"'
+      set-hook -g client-detached 'run-shell "tmux list-panes -t #{session_name} -F #{pane_pid} | xargs -I{} ps --ppid {} -o comm= | grep -vE \"^(zsh|yazi)$\" | grep -q . || tmux kill-session -t #{session_name}"'
 
       setw -g mode-keys vi
       
