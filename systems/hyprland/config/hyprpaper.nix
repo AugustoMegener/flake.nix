184c3a1fab380636ccec1e7752b6e5ea64c49be6
AugustoMegener <76854495+AugustoMegener@users.noreply.github.com>
2026-06-07 21:41:20 -0300

    nixos rebuild: switch

--- systems/hyprland/config/hyprpaper.nix
+++ systems/hyprland/config/hyprpaper.nix
@@ -3,11 +3,11 @@
   services.hyprpaper = {
     enable = true;
     settings = {
-      preload = [ "/home/kito/Pictures/Backgrounds/bg-1.png" ];
+      preload = [ "/home/kito/Pictures/Backgrounds/bg-1.jpg" ];
       wallpaper = [
         {
           monitor = "VGA-1";
-          path = "/home/kito/Pictures/Backgrounds/bg-1.png";
+          path = "/home/kito/Pictures/Backgrounds/bg-1.jpg";
         }
       ];
       splash = false;
