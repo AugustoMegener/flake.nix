984fc3d50d4a23078948bb062c3479abbe6bc3d0
AugustoMegener <76854495+AugustoMegener@users.noreply.github.com>
2026-06-11 14:20:42 -0300

    nixos rebuild: switch

--- home/programs/obs-studio.nix
+++ home/programs/obs-studio.nix
@@ -5,4 +5,12 @@
   home.packages = with pkgs; [
     obs-cli
   ];
+
+  xdg.desktopEntries.obs = {
+    name = "OBS Studio";
+    exec = "obs";
+    icon = "obs";
+    type = "Application";
+    categories = [ "AudioVideo" "Recorder" ];
+  };
 }
