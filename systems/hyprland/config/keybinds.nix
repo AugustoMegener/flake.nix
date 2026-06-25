5a43b7e2fbc1d5c6b54617a3ee7536c38f715ab0
AugustoMegener <76854495+AugustoMegener@users.noreply.github.com>
2026-06-25 16:44:30 -0300

    nixos rebuild: switch

--- systems/hyprland/config/keybinds.nix
+++ systems/hyprland/config/keybinds.nix
@@ -44,6 +44,7 @@
       "$mainMod SHIFT, S, movetoworkspace, special:magic"
       "$mainMod, mouse_down, workspace, e+1"
       "$mainMod, mouse_up, workspace, e-1"
+      "$mainMod, Escape, exec, qs ipc call powerMenu toggle"
       #"$mainMod, Escape, exec, systemctl hibernate"
       #"$mainMod SHIFT, Escape, exec, hyprshutdown --post-cmd 'systemctl poweroff'"
       #"$mainMod CTRL, Escape, exec, hyprshutdown --post-cmd 'systemctl reboot'"
