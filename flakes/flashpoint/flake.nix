5baf504619b114e1036fa7c17a76830ad9d4eef7
AugustoMegener <76854495+AugustoMegener@users.noreply.github.com>
2026-06-27 23:44:59 -0300

    nixos rebuild: switch

--- flakes/flashpoint/flake.nix
+++ flakes/flashpoint/flake.nix
@@ -8,71 +8,57 @@
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
 
-      pname = "flashpoint-data";
-      flashpointData = pkgs.stdenv.mkDerivation {
-      version = "14.0.3";
-
+    flashpointData = pkgs.stdenv.mkDerivation {
+      name = "flashpoint-data-14.0.3";
       src = pkgs.fetchurl {
         url = "https://download.unstable.life/upload/fp14.0.3_lin_20251201.7z";
         sha256 = "f393a98c5c35e229a744c102b0cb53270b1b4f1b3ebd40d604f98323444a4b1f";
       };
-
-      nativeBuildInputs = [ pkgs.p7zip ];
-
-      unpackPhase = ''
-        7z x $src -oflashpoint
-      '';
-
+      nativeBuildInputs = [ pkgs._7zz ];
+      unpackPhase = "7zz x $src -oflashpoint";
       installPhase = ''
         cp -r flashpoint $out
         find $out -name "*.sh" -exec chmod +x {} \;
       '';
-
       dontBuild = true;
       dontFixup = true;
     };
 
     flashpoint = pkgs.buildFHSEnv {
       name = "flashpoint";
-
-targetPkgs = p: with p; [
-  toybox
-  file
-  electron
-  pipewire
-  pulseaudio
-  gtk3
-  gtk2
-  nss
-  php
-  wine
-  libx11
-  libxt
-  libxcomposite
-  mesa
-  libGL
-  libGLU
-  glib
-  nspr
-  # at-spi2-atk  <- remove
-  # cups          <- remove
-  libdrm
-  pango
-  cairo
-  expat
-  libxkbcommon
-  alsa-lib
-  libxdamage
-  libxext
-  libxfixes
-  libxrandr
-  libxcb
-  udev
-];
-
+      targetPkgs = p: with p; [
+        toybox
+        file
+        electron
+        pipewire
+        pulseaudio
+        gtk3
+        gtk2
+        nss
+        php
+        libx11
+        libxt
+        libxcomposite
+        mesa
+        libGL
+        libGLU
+        glib
+        nspr
+        libdrm
+        pango
+        cairo
+        expat
+        libxkbcommon
+        alsa-lib
+        libxdamage
+        libxext
+        libxfixes
+        libxrandr
+        libxcb
+        udev
+      ];
       runScript = pkgs.writeShellScript "flashpoint-run" ''
         FP_DIR="''${FLASHPOINT_DIR:-$HOME/.local/share/flashpoint}"
-
         if [ ! -f "$FP_DIR/start-flashpoint.sh" ]; then
           echo "Inicializando Flashpoint em $FP_DIR ..."
           mkdir -p "$FP_DIR"
@@ -80,7 +66,6 @@ targetPkgs = p: with p; [
           chmod -R u+w "$FP_DIR"
           echo "Pronto."
         fi
-
         cd "$FP_DIR"
         exec ./start-flashpoint.sh --no-sandbox "$@"
       '';
@@ -88,15 +73,13 @@ targetPkgs = p: with p; [
   in
   {
     packages.${system} = {
-      default   = flashpoint;
+      default    = flashpoint;
       flashpoint = flashpoint;
     };
-
     apps.${system}.default = {
       type    = "app";
       program = "${flashpoint}/bin/flashpoint";
     };
-
     nixosModules.default = { ... }: {
       environment.systemPackages = [ flashpoint ];
     };
