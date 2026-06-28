64f0a32bd5df9fae5ada7f49160be0ebd2d3159f
AugustoMegener <76854495+AugustoMegener@users.noreply.github.com>
2026-06-28 13:41:37 -0300

    nixos rebuild: switch

--- home/programs/walker.nix
+++ home/programs/walker.nix
@@ -2,9 +2,9 @@
 {
   programs.walker.enable = true;
 
-xdg.configFile."walker/themes/screenshot/style.css".text = "";
+/*xdg.configFile."walker/themes/screenshot/style.css".text = "";
 xdg.configFile."walker/themes/screenshot/config.toml".text = ''
   [columns]
   "dmenu" = 3
-'';
+'';*/
 }
