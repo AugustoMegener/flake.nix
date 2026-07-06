71321aa24d18a2fc8ad17149649554a615d2c364
AugustoMegener <76854495+AugustoMegener@users.noreply.github.com>
2026-07-06 11:24:58 -0300

    fix(hyprpaper): let hyprpaper get bg-1.png instead of bg-1.jpg

--- systems/hyprland/config/hyprpaper.nix
+++ systems/hyprland/config/hyprpaper.nix
@@ -3,11 +3,11 @@
   services.hyprpaper = {
     enable = true;
     settings = {
-      preload = [ "/home/kito/Pictures/Backgrounds/bg-1.jpg" ];
+      preload = [ "/home/kito/Pictures/Backgrounds/bg-1.png" ];
       wallpaper = [
         {
           monitor = "VGA-1";
-          path = "/home/kito/Pictures/Backgrounds/bg-1.jpg";
+          path = "/home/kito/Pictures/Backgrounds/bg-1.png";
         }
       ];
       splash = false;
