40dac5f34d6d2bf1d2016f4fa2a54d532a2838e9
AugustoMegener <76854495+AugustoMegener@users.noreply.github.com>
2026-07-14 16:39:12 -0300

    AAAAAAAAAAAAAAAAAAAAAAAA

--- configuration.nix
+++ configuration.nix
@@ -221,6 +221,7 @@
 # You can use https://search.nixos.org/ to find more packages (and options).
 
   environment.systemPackages = with pkgs; [ 
+  direnv
     mesa-demos
     brightnessctl
     xdg-desktop-portal
