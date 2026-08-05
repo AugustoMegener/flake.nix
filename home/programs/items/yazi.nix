fb101e2d76210dfbbdab1793a009d6efea940242
AugustoMegener <76854495+AugustoMegener@users.noreply.github.com>
2026-08-05 13:55:25 -0300

    feat(yazi): wl-copy on yank

--- home/programs/items/yazi.nix
+++ home/programs/items/yazi.nix
@@ -131,14 +131,17 @@ in
           }
         ];
       };
-      mgr = {
-        prepend_keymap = [ 
-          {
-            on = "y";
-            run = [ "shell -- for path in %s; do echo "file://$path"; done | wl-copy -t text/uri-list" "yank" ];
-          }
-        ];
-      };
+mgr = {
+  prepend_keymap = [
+    {
+      on = "y";
+      run = [
+        ''shell -- for path in %s; do echo "file://$path"; done | wl-copy -t text/uri-list''
+        "yank"
+      ];
+    }
+  ];
+};
     };
 
     theme = {
